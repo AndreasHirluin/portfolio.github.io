95f03940288cc633c56e393fc4c01a937275e3a2
AndreasHirluin <masiaandreas@gmail.com>
2021-10-16 13:23:56 +0200

    JSON import fixed

--- script/level.js
+++ script/level.js
@@ -1,21 +1,30 @@
-document.addEventListener("DOMContentLoaded" ,()=>{
-    console.log("Ciao");
+document.addEventListener("DOMContentLoaded", ()=>{
+
     var skill = fetch('./../script/skills.json')
     .then(skills => skills.json());
-    var levelList = document.getElementsByClassName("level");
-    var levelElement = document.createElement("progress");
-    levelElement.setAttribute("max", 100);
-    console.log(levelList.length);
-    for(let i = 0; i < levelList.length; i++){
-        console.log(i);
-        let element = levelList[i].getElementsByClassName("skillname")[0].innerHTML.toUpperCase();
-        console.log(element);
-        skill.then(att => {
-            console.log(att[element].level);
-            levelElement.setAttribute("value", att[element].level);
-            levelList[i].appendChild(levelElement.cloneNode(true));
-        });
+
+    var skillList = document.getElementById("skills");
+    var progressElement = document.createElement("progress");
+    progressElement.setAttribute("max", 100);
         
-    }
+    skill.then(att => {
+        for(var obj in att){
+            let skillType = document.createElement("ul");
+            skillType.setAttribute("id", obj);
+            for(let item in att[obj]){                
+                let skillItem = document.createElement("li");
+                skillItem.setAttribute("class", "skillItem");
+                let skillName = document.createElement("h4");
+                skillName.setAttribute("class", "skillName");
+                skillName.innerHTML = att[obj][item].title;
+                progressElement.setAttribute("value", att[obj][item].level);
+                console.log(att[obj][item].title);
+                skillItem.appendChild(skillName.cloneNode(true));
+                skillItem.appendChild(progressElement.cloneNode(true));
+                skillType.appendChild(skillItem.cloneNode(true));
+            }
+            skillList.appendChild(skillType.cloneNode(true));
+        }
+    });
 });
 
